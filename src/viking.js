ac3f5c7286cdf9a409917600c49584ff3184d6f7
mirJakub <slawomir.krzyzak@gmail.com>
2023-11-24 17:29:03 +0100

    2nd commit, till saxon

--- src/viking.js
+++ src/viking.js
@@ -11,7 +11,6 @@ class Soldier {
         this.health = this.health - damage
     }
 }
-
 const newSoldier = new Soldier(50, 100);
 console.log(newSoldier.attack());
 
@@ -19,18 +18,49 @@ console.log(newSoldier.attack());
 // Viking
 class Viking extends Soldier {
     constructor(name, health, strength) {
+        super(health, strength);
         this.name = name;
         this.health = health;
         this.strength = strength;
-        // super (health, strength, name)
     }
     attack() {
         return this.strength
     }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        if (this.health > 0) {
+            return `${this.name} has received ${this.damage} points of damage`
+        }
+        else {
+            return `${this.name} has died in act of combat`
+        }
+
+    }
+    battleCry() {
+        return `Odin Owns You All!`
+    }
 }
 
+const newViking = new Viking(100, 100, "Taron");
+
 // Saxon
-class Saxon { }
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        if (this.health > 0) {
+            return `A Saxon has received ${this.damage} points of damage`
+        }
+        else {
+            return `A Saxon has died in act of combat`
+        }
+
+    }
+}
 
 // War
 class War { }
