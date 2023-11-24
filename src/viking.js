ead8119c76c996e29ea6848d00049e4bf15bb2de
mirJakub <slawomir.krzyzak@gmail.com>
2023-11-24 17:06:17 +0100

    first commit friday

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,36 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+    }
+}
+
+const newSoldier = new Soldier(50, 100);
+console.log(newSoldier.attack());
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        // super (health, strength, name)
+    }
+    attack() {
+        return this.strength
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
