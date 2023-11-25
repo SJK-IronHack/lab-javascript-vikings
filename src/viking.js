139aff42432232b57c4d59eca6811066343663ea
mirJakub <slawomir.krzyzak@gmail.com>
2023-11-25 16:38:41 +0100

    done,  error With return of string in receive damage

--- src/viking.js
+++ src/viking.js
@@ -90,6 +90,32 @@ class War {
         if (randomSaxon.health <= 0) {
             this.saxonArmy.splice(this.saxonArmy.indexOf(randomSaxon), 1);
         }
+        return result
     }
+    saxonAttack() {
+        const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        const damage = randomSaxon.attack();
+        const result = randomViking.receiveDamage(damage);
+        if (randomViking.health <= 0) {
+            this.vikingArmy.splice(this.vikingArmy.indexOf(randomViking), 1);
+        }
+        return result
+    }
+    showStatus() {
+        if (this.saxonArmy.length === 0) {
+            return `Vikings have won the war of the century!`
+        }
+        if (this.vikingArmy.length === 0) {
+            return `Saxons have fought for their lives and survived another day...`
+        }
+        if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+            return `Vikings and Saxons are still in the thick of battle.`
+        }
+
+    }
+
+
 }
+
 
